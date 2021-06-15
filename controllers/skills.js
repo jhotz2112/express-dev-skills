8e8a03e28b25d8af529e572f97ca4be28a3259d1
Jeremy Hotz <JeremyHotz@Jeremys-iMac-2.local>
2021-06-15 17:14:18 -0600

    codemon still crashing

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,10 +3,12 @@ module.exports = {
   index,
   show
 };
+
 function show(req, res) {
   const skill = Skill.getOne(req.params.id);
   res.render('skills/show', { skill });
 }
+
 function index(req, res) {
   const skills = Skill.getAll();
   res.render('skills/index', { skills });
